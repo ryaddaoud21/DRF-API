fe2e35d65bf7b8b90dcb1c71994aa1c72c1d5ba9
Mohamed Riyad <65093859+ryaddaoud21@users.noreply.github.com>
2022-04-10 01:37:18 +0200

    'hellohek'

--- DRF_API/settings.py
+++ DRF_API/settings.py
@@ -42,7 +42,11 @@ REST_FRAMEWORK = {
     )
 }
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
